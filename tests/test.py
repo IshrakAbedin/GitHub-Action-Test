c79f5ceb279579df16b73e163ceacd451d370db3
Ishrak <ishrak.abedin.a1@gmail.com>
2020-12-03 13:58:46 +0600

    Modified Python

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,26 @@
 import subprocess
 
-def log(msg : str):
-    print("[Python Tester]:", msg)
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    ITALICIZED = '\033[3m'
+    UNDERLINED = '\033[4m'
+    INVERTED = '\033[7m'
 
-log("Beginning Test")
-try:
-    subprocess.check_call(["./bin/out", "Hello", "GitHub", "Action", ", Yey!"])
-    log(f"Process Completed Successfull with Return Code <0>")
-except subprocess.CalledProcessError as error:
-    log(f"Execution Failed with Returned Code <{error.returncode}>")
+def log(msg : str):
+    print(f"[{bcolors.OKBLUE}Python Tester{bcolors.ENDC}]:", msg)
+
+def main():
+    log("Beginning Tests")
+    try:
+        subprocess.check_call(["./bin/out", "Hello", "GitHub", "Action", ", Yey!"])
+        log(f"Process Completed Successfull with Return Code <0>")
+    except subprocess.CalledProcessError as error:
+        log(f"Execution Failed with Returned Code <{error.returncode}>")
+
+main()
