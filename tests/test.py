a203ca9cda672c977528b3dcc787223efc499ab7
Ishrak <ishrak.abedin.a1@gmail.com>
2020-12-03 13:12:23 +0600

    Initial Commit

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def log(msg : str):
+    print("[Python Tester]:", msg)
+
+log("Beginning Test")
+try:
+    subprocess.check_call(["./bin/out.exe", "Hello", "GitHub", "Action", ", Yey!"])
+    log(f"Process Completed Successfull with Return Code <0>")
+except subprocess.CalledProcessError as error:
+    log(f"Execution Failed with Returned Code <{error.returncode}>")
